19313ee5480f681bab76799f5d053f3351b06aa0
baseon2016 <baseon_000@163.com>
2019-08-29 10:26:12 +0800

    Updates

---
+++ precache-manifest.603a04fb0eca2bffd6935b3f84710bdd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6e55e4598955721eca35113e2189d9",
+    "url": "/react-yiji/index.html"
+  },
+  {
+    "revision": "c06bb6a54f2dde6e4cfd",
+    "url": "/react-yiji/static/css/2.acb72abc.chunk.css"
+  },
+  {
+    "revision": "fe3291d860f87bdb14c6",
+    "url": "/react-yiji/static/css/main.7d57de0b.chunk.css"
+  },
+  {
+    "revision": "c06bb6a54f2dde6e4cfd",
+    "url": "/react-yiji/static/js/2.6c46fe1a.chunk.js"
+  },
+  {
+    "revision": "fe3291d860f87bdb14c6",
+    "url": "/react-yiji/static/js/main.da72611d.chunk.js"
+  },
+  {
+    "revision": "76e3e25619ead56721a2",
+    "url": "/react-yiji/static/js/runtime~main.df17a495.js"
+  },
+  {
+    "revision": "228899d355eabb54c26b3532a5950bc5",
+    "url": "/react-yiji/static/media/iconfont.228899d3.svg"
+  },
+  {
+    "revision": "3258849a4a411cadb7c56d1a9cf33580",
+    "url": "/react-yiji/static/media/iconfont.3258849a.eot"
+  },
+  {
+    "revision": "d5b6b4778a235c853c6c737306bc1860",
+    "url": "/react-yiji/static/media/iconfont.d5b6b477.woff"
+  },
+  {
+    "revision": "dfa01cd3c1160904162a6d7580801d35",
+    "url": "/react-yiji/static/media/iconfont.dfa01cd3.ttf"
+  }
+]);
